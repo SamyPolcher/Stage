0ea32de524eb047206a085026b7b753fac4dac6e
MarieHartmann <67277937+MarieHartmann@users.noreply.github.com>
2020-07-07 15:16:15 +0200

    Update BDT_1.py

--- BDT_1.py
+++ BDT_1.py
@@ -69,18 +69,19 @@ if __name__ == '__main__':
     sig = ut.open('/users/LHCb/polcherrafael/MC/MC_BKGCAT10.root')["t"]
     bkg = ut.open('/users/LHCb/polcherrafael/Data/Data_Bruit.root')["t"]
 
+    beta = np.divide( Jpsi_P - Proton_P - Kaon_P ,  Jpsi_P + Proton_P + Kaon_P )
     minHadron_IPCHI2 = np.minimum(Proton_IPCHI2_OWNPV,Kaon_IPCHI2_OWNPV)
     minHadron_PT = np.minimum(Proton_PT,Kaon_PT)
-    sumHadron_PT = np.sum(Proton_PT,Kaon_PT)
-    sumHadron_IPCHI2 = np.sum(Proton_IPCHI2_OWNPV,Kaon_IPCHI2_OWNPV)
+    sumHadron_PT = Proton_PT + Kaon_PT
+    sumHadron_IPCHI2 = Proton_IPCHI2_OWNPV + Kaon_IPCHI2_OWNPV)
     minLepton_PT = np.min(L1_PT,L2_PT)
     maxLepton_IPCHI2 = np.max(L1_IPCHI2_OWNPV,L2_IPCHI2_OWNPV)
     minLepton_IPCHI2 = np.min(L1_IPCHI2_OWNPV,L2_IPCHI2_OWNPV)
-    sumLJ_PT = np.sum(Lambdastar_PT,Jpsi_PT)
-    Hsum_ETA = np.sum(Proton_ETA,Kaon_ETA)
+    sumLJ_PT = Lambdastar_PT + Jpsi_PT
+    Hsum_ETA = Proton_ETA + Kaon_ETA
     
     # define the variables we want to use
-    list_vars = ["Log_Lb_PT", "Log_Lb_IPCHI2_OWNPV", 'Log_Lb_DIRA_OWNPV', "Lb_DIRA_OWNPV", 
+    list_vars = ["beta", "Log_Lb_PT", "Log_Lb_IPCHI2_OWNPV", 'Log_Lb_DIRA_OWNPV', "Lb_DIRA_OWNPV", 
     "Log_Lb_FDCHI2_OWNPV", "Log_Lb_LOKI_DTF_CHI2NDOF", "Log_Lb_ENDVERTEX_CHI2", 
     "Log_Jpsi_FDCHI2_OWNPV", "Log_Lambdastar_PT", "Log_Lambdastar_IPCHI2_OWNPV", "Log_Lambdastar_ENDVERTEX_CHI2", 
     "minHadron_PT", "minHadron_IPCHI2", "sumHadron_PT","sumHadron_IPCHI2", "minLepton_PT", "maxLepton_IPCHI2", "minLepton_IPCHI2","sumLJ_PT", 
