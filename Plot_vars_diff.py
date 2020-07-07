844786eaa2875e26f6da979fdaed10920d463433
MarieHartmann <67277937+MarieHartmann@users.noreply.github.com>
2020-07-07 14:50:12 +0200

    Update Plot_vars_diff.py

--- Plot_vars_diff.py
+++ Plot_vars_diff.py
@@ -10,23 +10,21 @@ Signal = ut.open('/users/LHCb/polcherrafael/MC/MC_BKGCAT10.root')["t"]
 #data = ut.open('/users/LHCb/polcherrafael/Data/Select_sig.root')["t"]
 
 minHadron_IPCHI2 = np.minimum(Proton_IPCHI2_OWNPV,Kaon_IPCHI2_OWNPV)
-minHadron_PT = 
-sumHadron_PT = np.sum(Proton_PT,Kaonn_PT
-sumHadron_IPCHI2 = Proton_IPCHI2_OWNPV + Kaon_IPCHI2_OWNPV
-minLepton_PT = 
-maxLepton_IPCHI2 = 
-minLepton_IPCHI2 = 
-sumLJ_PT = Lambdastar_PT + Jpsi_PT
-Hsum_ETA = Proton_ETA + Kaon_ETA
-
+minHadron_PT = np.minimum(Proton_PT,Kaon_PT)
+sumHadron_PT = np.sum(Proton_PT,Kaon_PT)
+sumHadron_IPCHI2 = np.sum(Proton_IPCHI2_OWNPV,Kaon_IPCHI2_OWNPV)
+minLepton_PT = np.min(L1_PT,L2_PT)
+maxLepton_IPCHI2 = np.max(L1_IPCHI2_OWNPV, L2_IPCHI2_OWNPV)
+minLepton_IPCHI2 = np.min(L1_IPCHI2_OWNPV, L2_IPCHI2_OWNPV)
+sumLJ_PT = np.sum(Lambdastar_PT,Jpsi_PT)
+Hsum_ETA = np.sum(Proton_ETA,Kaon_ETA)
 
 
 list_vars = ["Log_Lb_PT", "Log_Lb_IPCHI2_OWNPV", 'Log_Lb_DIRA_OWNPV', "Lb_DIRA_OWNPV", 
-"Log_Lb_FDCHI2_OWNPV", "Log_Lb_LOKI_DTF_CHI2NDOF",
-"Log_Lb_ENDVERTEX_CHI2", "Log_Jpsi_FDCHI2_OWNPV", "Log_Lambdastar_PT", "Log_Lambdastar_IPCHI2_OWNPV",
-"Log_Lambdastar_ENDVERTEX_CHI2", 
-#"minHadron_PT", "minHadron_IPCHI2", "sumHadron_PT","sumHadron_IPCHI2", "minLepton_PT", "maxLepton_IPCHI2", "minLepton_IPCHI2","sumLJ_PT", 
-"Log_Proton_P",] #"Hsum_ETA"]
+"Log_Lb_FDCHI2_OWNPV", "Log_Lb_LOKI_DTF_CHI2NDOF", "Log_Lb_ENDVERTEX_CHI2", 
+"Log_Jpsi_FDCHI2_OWNPV", "Log_Lambdastar_PT", "Log_Lambdastar_IPCHI2_OWNPV", "Log_Lambdastar_ENDVERTEX_CHI2", 
+"minHadron_PT", "minHadron_IPCHI2", "sumHadron_PT","sumHadron_IPCHI2", "minLepton_PT", "maxLepton_IPCHI2", "minLepton_IPCHI2","sumLJ_PT", 
+"Log_Proton_P","Hsum_ETA"]
 
 def plot_vars(list_vars) :
     page = math.ceil(len(list_vars)/6)
