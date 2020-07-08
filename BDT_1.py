c3430e5e25534de69156c4ef2ba0c8c77205419f
MarieHartmann <67277937+MarieHartmann@users.noreply.github.com>
2020-07-08 17:06:09 +0200

    Update BDT_1.py

--- BDT_1.py
+++ BDT_1.py
@@ -9,7 +9,7 @@ from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 
 def plot_output(y_train, y_test, y_prob_train, y_prob_test,
-                figname='overtraining_MS.png'):
+                figname='overtraining_MS_vars.png'):
     """Plot train and test BDT output for signal and bkg.
     :param y_train: train labels
     :type y_train: array
@@ -41,8 +41,10 @@ def plot_output(y_train, y_test, y_prob_train, y_prob_test,
     # test: error bars
     test_sig = y_prob_test[y_test==1][:,1] 
     hist_sig, bins_sig = np.histogram(test_sig,
-                                      range=range, bins=nbins,
-                                      density=True)
+                                      range=range, bins=nbins, density=True)
+    #print (test_sig)
+    #print (bins_sig)
+    #print (hist_sig)
     cent_sig = (bins_sig[1:] + bins_sig[:-1])/2
     plt.errorbar(cent_sig, hist_sig, fmt='o',
                  color="red", label="Sig Test") 
@@ -66,30 +68,23 @@ def plot_output(y_train, y_test, y_prob_train, y_prob_test,
 
 if __name__ == '__main__':
     # read the data
-    sig = ut.open('/users/LHCb/polcherrafael/MC/MC_BKGCAT10.root')["t"]
-    bkg = ut.open('/users/LHCb/polcherrafael/Data/Data_Bruit.root')["t"]
+    sig = ut.open('/users/LHCb/polcherrafael/MC/MC_BKGCAT10_vars.root')["t"]
+    bkg = ut.open('/users/LHCb/polcherrafael/Data/Data_Bruit_vars.root')["t"]
 
-    beta = np.divide( Jpsi_P - Proton_P - Kaon_P ,  Jpsi_P + Proton_P + Kaon_P )
-    minHadron_IPCHI2 = np.minimum(Proton_IPCHI2_OWNPV,Kaon_IPCHI2_OWNPV)
-    minHadron_PT = np.minimum(Proton_PT,Kaon_PT)
-    sumHadron_PT = Proton_PT + Kaon_PT
-    sumHadron_IPCHI2 = Proton_IPCHI2_OWNPV + Kaon_IPCHI2_OWNPV)
-    minLepton_PT = np.min(L1_PT,L2_PT)
-    maxLepton_IPCHI2 = np.max(L1_IPCHI2_OWNPV,L2_IPCHI2_OWNPV)
-    minLepton_IPCHI2 = np.min(L1_IPCHI2_OWNPV,L2_IPCHI2_OWNPV)
-    sumLJ_PT = Lambdastar_PT + Jpsi_PT
-    Hsum_ETA = Proton_ETA + Kaon_ETA
-    
     # define the variables we want to use
-    list_vars = ["beta", "Log_Lb_PT", "Log_Lb_IPCHI2_OWNPV", 'Log_Lb_DIRA_OWNPV', "Lb_DIRA_OWNPV", 
-    "Log_Lb_FDCHI2_OWNPV", "Log_Lb_LOKI_DTF_CHI2NDOF", "Log_Lb_ENDVERTEX_CHI2", 
-    "Log_Jpsi_FDCHI2_OWNPV", "Log_Lambdastar_PT", "Log_Lambdastar_IPCHI2_OWNPV", "Log_Lambdastar_ENDVERTEX_CHI2", 
-    "minHadron_PT", "minHadron_IPCHI2", "sumHadron_PT","sumHadron_IPCHI2", "minLepton_PT", "maxLepton_IPCHI2", "minLepton_IPCHI2","sumLJ_PT", 
-    "Log_Proton_P","Hsum_ETA"]
+    #vars = ["Lb_PT","Lb_IPCHI2_OWNPV","Lb_FDCHI2_OWNPV","Lb_LOKI_DTF_CHI2NDOF", "Lb_ENDVERTEX_CHI2","Jpsi_FDCHI2_OWNPV", "Lambdastar_PT", "Lambdastar_IPCHI2_OWNPV", "Lambdastar_ENDVERTEX_CHI2", "Proton_P"]
+
+    #vars = ["Lb_IPCHI2_OWNPV", "Lb_ENDVERTEX_CHI2", "Lambdastar_IPCHI2_OWNPV", "Lambdastar_ENDVERTEX_CHI2"]
     
+    vars = ["beta", "Lb_PT", "Lb_IPCHI2_OWNPV", 'Lb_DIRA_OWNPV', "Lb_DIRA_OWNPV", 
+    "Lb_FDCHI2_OWNPV", "Lb_LOKI_DTF_CHI2NDOF", "Lb_ENDVERTEX_CHI2", 
+    "Jpsi_FDCHI2_OWNPV", "Lambdastar_PT", "Lambdastar_IPCHI2_OWNPV", "Lambdastar_ENDVERTEX_CHI2", 
+    "minHadron_PT", "minHadron_IPCHI2", "sumHadron_PT","sumHadron_IPCHI2", "minLepton_PT", "maxLepton_IPCHI2", "minLepton_IPCHI2","sumLJ_PT", 
+    "Proton_P","Hsum_ETA"]
+
     # create a pandas data frame with these variables only
-    sig_array = sig.pandas.df(vars).to_numpy() # use only 1000 events for this example!
-    bkg_array = bkg.pandas.df(vars).to_numpy() # use only 1000 events for this example!
+    sig_array = sig.pandas.df(vars).to_numpy()
+    bkg_array = bkg.pandas.df(vars).to_numpy()
     print("Signal shape:", sig_array.shape)
     print("Backgr shape:", bkg_array.shape)
 
@@ -105,23 +100,29 @@ if __name__ == '__main__':
 
     # define a model, example: xgboost
     params_dict = {
-        "n_estimators": 200,
+        "n_estimators": 100,
         "learning_rate": 0.3,
-        "max_depth": 3,
+        "max_depth": 2,
         }
     model = xgboost.XGBClassifier(**params_dict)
 
     # train it
+    #print (y_train)
     model.fit(X_train, y_train)
     print("Model has been trained")
+    #print (y_train)
 
     # plot features importance
     xgboost.plot_importance(model)
-    plt.savefig("vars_ranking_MS.png")
+    plt.savefig("vars_ranking_MS_vars.png")
+
+    #xgboost.plot_tree(model)
+    #plt.savefig("representation_arbre")
 
     # make predictions
     y_prob_train = model.predict_proba(X_train)
     y_prob_test  = model.predict_proba(X_test)
+    #print (y_prob_train)
 
     # plot train and test output
     fig1 = plot_output(y_train, y_test, y_prob_train, y_prob_test)
